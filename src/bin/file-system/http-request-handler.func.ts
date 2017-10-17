b445cd1fbbd33a436b23668c1fc3bb5821b498c1
Jason Morley <Jason.Morley@zibrant.com>
2017-10-17 13:45:13 +0100

    Refactored file-system 'middleware'

--- src/bin/file-system/http-request-handler.func.ts
+++ src/bin/file-system/http-request-handler.func.ts
@@ -1,5 +1,8 @@
+import { stat } from "fs";
 import { List, Map } from "immutable";
 import { join } from "path";
+import { Observer } from "rxjs/Observer";
+import { Observable } from "rxjs/Rx";
 
 import { HttpRequestHandler } from "../server/http-request-handler.type";
 import { mimeTypes } from "./mime-types";
@@ -10,15 +13,40 @@ const getMimeType = (filename: string): string[] => mimeTypes["." + (filename.sp
 const fileRoot = join(__dirname, "../../../dist/www");
 
 export const onFileRequest: HttpRequestHandler = request$ => request$
-	.map(request => join(fileRoot, request.url || "/"))
-	.mergeMap(targetFile => {
-		return render(targetFile)
-			.concat(render(join(targetFile, "index.html")))
-			.concat(render(join(fileRoot, "index.html")))
-			.take(1)
-			.map(({ data, filename }) => ({
-				status: 200,
-				body: data,
-				headers: Map({ "Content-Type": List(getMimeType(filename)) })
-			}));
+	.map(request => request.url || "/")
+	.mergeMap(tryGetRequestFilePath)
+	.mergeMap(targetFile => render(targetFile))
+	.map(({ data, filename }) => ({
+		status: 200,
+		body: data,
+		headers: Map({
+			"Content-Type": List(...getMimeType(filename), "charset=UTF-8"),
+			"Content-Length": List(data.length.toString())
+		})
+	}));
+
+const tryGetRequestFilePath = (url: string): Observable<string> => Observable.create((observer: Observer<string>) => {
+	const shallowFile = join(fileRoot, url);
+	stat(shallowFile, (err, shallowStats) => {
+		if (err) {
+			observer.complete();
+		} else if (shallowStats.isFile()) {
+			observer.next(shallowFile);
+			observer.complete();
+		} else if (shallowStats.isDirectory()) {
+			const deepFile = join(shallowFile, "index.html");
+			stat(join(shallowFile, "index.html"), (err, deepState) => {
+				if (err) {
+					observer.complete();
+				} else if (deepState.isFile()) {
+					observer.next(deepFile);
+					observer.complete();
+				} else {
+					observer.complete();
+				}
+			});
+		} else {
+			observer.complete();
+		}
 	});
+});
